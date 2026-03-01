0811d58af588e90adf5c6305ab9bb6d266423cef
CaletCambronero <162659315+CaletCambronero@users.noreply.github.com>
2026-02-28 19:38:22 -0600

    Delete Caso-de-estudio directory

--- Caso-de-estudio/src/main/java/org/example/Modelo/Curso.java
+++
@@ -1,7 +0,0 @@
-package org.example.Modelo;
-
-public class Curso extends ElementoAcademico {
-    public Curso(int id, String nombre, String descripcion, String estado) {
-        super(id, nombre, descripcion, estado);
-    }
-}
--- Caso-de-estudio/src/main/java/org/example/Modelo/Grupo.java
+++
@@ -1,7 +0,0 @@
-package org.example.Modelo;
-
-public class Grupo extends ElementoAcademico {
-    public Grupo(int id, String nombre, String descripcion, String estado) {
-        super(id, nombre, descripcion, estado);
-    }
-}
